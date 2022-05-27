b1c9c12d7ad818396d9752177f108ae10945fe1c
noor-s-h <hi@noor.sh>
2022-05-27 09:20:28 +0000

    back to tailwind media query

--- components/Layout/Header/AuthBtn.jsx
+++ components/Layout/Header/AuthBtn.jsx
@@ -1,19 +1,15 @@
 import Link from "components/Link"
-import useMediaQuery from "lib/useMediaQuery";
 
 export default function AuthBtn() {
-    const isDesktop = useMediaQuery('(min-width: 768px)');
 
     return (
         <div className="auth-button">
             <Link href="/login">
                     Login
             </Link>
-            {isDesktop && 
-                <Link linkClass="ml-4 inline-block text-gray-50 text-sm bg-gray-400 px-5 py-2 rounded-full" href="#">
-                    Signup
-                </Link>
-            }
+            <Link linkClass="ml-4 hidden md:inline-block text-gray-50 text-sm bg-gray-400 px-5 py-2 rounded-full" href="#">
+                Signup
+            </Link>
         </div>
     )
 }
--- components/Layout/Header/index.jsx
+++ components/Layout/Header/index.jsx
@@ -7,12 +7,10 @@ import { useRouter } from 'next/router';
 import DeskTopMenu from './DeskTopMenu'
 import AuthBtn from './AuthBtn'
 import Close from 'components/icons/Close'
-import useMediaQuery from "lib/useMediaQuery";
 
 export default function NavBar() {
     const [open, setOpen] = useState(false)
     const { pathname } = useRouter();
-    const isDesktop = useMediaQuery('(min-width: 768px)');
 
     const navOpen = () => {
         setOpen(true)
@@ -29,37 +27,32 @@ export default function NavBar() {
     return (
         <header className="border-b">
             <nav className="navbar container max-w-[50rem] mx-auto px-4 flex filter bg-white h-[56px] items-center justify-between">
-                {!isDesktop && 
-                    <div className='hamburger-button'>
-                        {open ?
-                            <Close iconClass="h-5 w-5 text-gray-500" setOpen={navClose} />
-                            :
-                            <Menu iconClass="h-5 w-5 text-gray-500" setOpen={navOpen} />
-                        }
-                    </div>
-                }
+                <div className='hamburger-button md:hidden'>
+                    {open ?
+                        <Close iconClass="h-5 w-5 text-gray-500" setOpen={navClose} />
+                        :
+                        <Menu iconClass="h-5 w-5 text-gray-500" setOpen={navOpen} />
+                    }
+                </div>
                 <Logo />
-                {isDesktop ?
-                    <ul className="desktop-menu flex">
-                        {
-                            NavData.map(({ title, label, href, childrens }, index) => {
-                                return (
-                                    <DeskTopMenu key={index} title={title} label={label} href={href} childrens={childrens} />
-                                )
-                            })
-                        }
-                    </ul> 
-                    :
-                    <ul className={`mobile-menu z-50 overflow-hidden absolute top-[57px] p-4 left-0 right-0 bg-white transition-transform ease-[cubic-bezier(1, 0.5, 1, 0.25))] ${open ? "h-auto translate-y-0 visible border-b" : "h-0 translate-y-1 invisible"}`}>
-                        {open &&
-                            NavData.map(({ title, label, href, childrens }, index) => {
-                                return (
-                                    <MobileMenu key={index} title={title} label={label} href={href} childrens={childrens} />
-                                )
-                            })
-                        }
-                    </ul>
-                }
+                <ul className={`mobile-menu md:hidden z-50 overflow-hidden absolute top-[57px] p-4 left-0 right-0 bg-white transition-transform ease-[cubic-bezier(1, 0.5, 1, 0.25))] ${open ? "h-auto translate-y-0 visible border-b" : "h-0 translate-y-1 invisible"}`}>
+                    {open &&
+                        NavData.map((navdata, index) => {
+                            return (
+                                <MobileMenu key={index} {...navdata} />
+                            )
+                        })
+                    }
+                </ul>
+                <ul className="desktop-menu hidden md:flex">
+                    {
+                        NavData.map((navdata, index) => {
+                            return (
+                                <DeskTopMenu key={index} {...navdata} />
+                            )
+                        })
+                    }
+                </ul> 
                 <AuthBtn />
             </nav>
         </header>
